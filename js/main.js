3011fcbe35794a0caa97e045c6bc1fe48f672fbe
sadakosa <sabrinachow98@gmail.com>
2018-05-28 19:16:32 +0800

    added logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log('User flipped ' + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("you found a match!")
+} else {
+	alert("please try again D:")
+}
